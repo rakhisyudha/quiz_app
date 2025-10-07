a7dc0287571953f244c1ff320d9ef3d086d02161
rakhisdeyudha <rakhisyuda@gmail.com>
2025-10-07 18:18:11 +0700

    update to shuffled list

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,3 +1,5 @@
+import 'package:adv_app/answer_button.dart';
+import 'package:adv_app/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class QuestionScreen extends StatefulWidget {
@@ -11,6 +13,27 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(context) {
-    return Center(child: const Text("Question Page"));
+    final currentQuestion = questions[0];
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAndswer().map((answer) {
+              return AnswerButton(answerText: answer, onTap: () {});
+            }),
+          ],
+        ),
+      ),
+    );
   }
 }
